95c243c8116e4e6804673126afbfefbf2d440c64
sunetra-git <60608213+sunetra-git@users.noreply.github.com>
2020-02-19 02:02:37 +0530

    Update Division.java

--- Division.java
+++ Division.java
@@ -1,9 +1,9 @@
-public class DivisionTest 
+public class Division 
 {
 		private int firstOperand;
 		private int secondOperand;
 		private int result = 0;
-		public DivisionTest(int firstOperand, int secondOperand) {
+		public Division(int firstOperand, int secondOperand) {
 			this.firstOperand = firstOperand;
 			this.secondOperand = secondOperand;
     }
